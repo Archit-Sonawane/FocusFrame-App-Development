68d36a54b4eaca3463e07559d658a5c32f0ebc64
Archit-Sonawane <archeetstudy@tamu.edu>
2025-04-03 10:49:07 -0500

    Initial Commit

---
+++ FocusFrame/FocusFrameApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusFrameApp.swift
+//  FocusFrame
+//
+//  Created by Sonawane, Archit Parag on 4/3/25.
+//
+
+import SwiftUI
+
+@main
+struct FocusFrameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FocusFrameTests/FocusFrameTests.swift
@@ -0,0 +1,17 @@
+//
+//  FocusFrameTests.swift
+//  FocusFrameTests
+//
+//  Created by Sonawane, Archit Parag on 4/3/25.
+//
+
+import Testing
+@testable import FocusFrame
+
+struct FocusFrameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
